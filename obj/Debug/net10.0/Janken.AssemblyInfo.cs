7039d9cce0542a56f83c6ad79a7b61f946bde010
ando.yoshie <ando.yoshie1111@gmail.com>
2026-05-19 13:57:36 +0900

    README修正

--- obj/Debug/net10.0/Janken.AssemblyInfo.cs
+++ obj/Debug/net10.0/Janken.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Janken")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4bd5033ed53934ff2072d4f47e4389d31977869d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Janken")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Janken")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
